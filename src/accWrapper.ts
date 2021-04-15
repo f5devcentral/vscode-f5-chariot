463168fab81e2f2acb826196df4a8966b3eee284
DumpySquare <bunookana@gmail.com>
2021-04-15 12:19:01 +0000

    refactored for acc v1.11.0

---
+++ src/accWrapper.ts
@@ -0,0 +1,151 @@
+/**
+ * Copyright 2021 F5 Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 'use strict';
+
+// import fs = require('fs');
+const analytics = require('../acc/src/analytics');
+const as3Properties = require('../acc/src/maps/as3-properties-latest');
+const as3PropertiesCustom = require('../acc/src/maps/as3-properties-custom');
+const converter = require('../acc/src/converter');
+const countObjects = require('../acc/src/util/countObjects');
+const declarationStats = require('../acc/src/declarationStats');
+const extract = require('../acc/src/extract');
+const filterConf = require('../acc/src/filterConf');
+const filterByApplication = require('../acc/src/filterByApplication');
+const getBigipVersion = require('../acc/src/getBigipVersion');
+const getConfigFiles = require('../acc/src/getConfigFiles');
+const logObjects = require('../acc/src/logObjects');
+const parse = require('../acc/src/parse');
+const readFiles = require('../acc/src/readFiles');
+const removeDefaultValues = require('../acc/src/removeDefaultValues');
+const removeIapp = require('../acc/src/removeIapp');
+const supported = require('../acc/src/maps/customDict');
+
+
+
+
+export async function acc (data: any) {
+
+    const config = {
+        ucs: false,
+        container: false,
+        disableAnalytics: false,
+        recognized: true,
+        recognizedObjects: true,
+        supported: true,
+        supportedObjects: true,
+        unsupported: true,
+        unsupportedObjects: true,
+        summary: true,
+        debug: true,
+        showExtended: true
+    };
+
+    // let files = [];
+
+    // // Extract from UCS
+    // if (config.ucs) {
+    //     files = await getConfigFiles(config.ucs);
+    // }
+
+    // // parse input into JSON
+    // const data = config.conf ? readFiles([config.conf]) : readFiles(files);
+
+    const json = parse({ "config.conf": data });
+
+    // extend as3Properties with custom
+    const as3PropertiesExt = Object.assign({}, as3Properties, as3PropertiesCustom);
+
+    // apply whitelist for AS3 and Charon support
+    const as3Json = filterConf(json, as3PropertiesExt);
+    const supportedJson = filterConf(json, supported);
+
+   // Clean up supported and AS3: remove iapp objects
+   const unsupportedObj = removeIapp(as3Json, supportedJson);
+
+    // convert json to AS3
+    const converted = converter(json, config);
+    const declaration = converted.decl;
+
+    // Filter by virtual server name
+    // if (config.vsName) {
+    //     declaration = filterByApplication(declaration, config);
+    // }
+
+    converted.unsupportedObjects.forEach((obj: any) => Object.assign(unsupportedObj, obj));
+    
+    const jsonDeclaration = JSON.stringify(declaration, null, 4);
+
+    // if (!config.showExtended) {
+    //     declaration = removeDefaultValues(declaration);
+    // }
+
+    const declarationInfo = declarationStats(declaration);
+    const declarationInfoTotal = declarationInfo.total;
+    const declarationInfoClasses = declarationInfo.classes;
+    const inputType = config.ucs ? 'ucs' : 'conf/scf';
+
+    // Count objects for statistic purposes
+    const jsonCount = countObjects(json);
+    const supportedJsonCount = countObjects(supportedJson);
+    const as3JsonCount = countObjects(as3Json);
+
+    // Send analitics
+    const bigipVersion = getBigipVersion(data);
+    if (!config.disableAnalytics) {
+        await analytics({
+            arguments: process.argv,
+            configInputSize: JSON.stringify(data).length,
+            container: config.container,
+            declarationOutputSize: JSON.stringify(declaration).length,
+            declarationStats: declarationInfo.classes,
+            inputType,
+            objectsACCSupported: supportedJsonCount,
+            objectsAS3Supported: as3JsonCount,
+            tmshVersion: bigipVersion,
+            totalObjectsDetected: jsonCount,
+            unsupportedTypes: converted.unsupportedAnonymized
+        });
+    }
+
+    // // remove extracted
+    // if (config.ucs) extract.cleanup();
+    
+    // logObjects({
+    //     config,
+    //     jsonCount,
+    //     as3Json,
+    //     as3JsonCount,
+    //     jsonDeclaration,
+    //     supportedJson,
+    //     supportedJsonCount,
+    //     declarationInfoTotal,
+    //     declarationInfoClasses,
+    //     unsupportedObj
+    // });
+
+    return {
+        declaration,
+        metaData: {
+            recognized: as3Json,
+            supported: supportedJson,
+            unSupported: unsupportedObj,
+            declarationInfo,
+        }
+    }
+    // return jsonDeclaration;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,83 @@
-/*
- * Copyright 2020. F5 Networks, Inc. See End User License Agreement ("EULA") for
- * license terms. Notwithstanding anything to the contrary in the EULA, Licensee
- * may copy and modify this software product for its internal business purposes.
- * Further, Licensee may upload, publish and distribute the modified version of
- * the software product on devcentral.f5.com.
+/**
+ * Copyright 2021 F5 Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 'use strict';
 
-import { 
+import {
     ExtensionContext,
     commands,
     window,
     ProgressLocation
 } from 'vscode';
 
-import logger from './logger';
-import { 
+import {
     displayJsonInEditor,
     getText
 } from './util';
 
-const acc = require('../acc/src/accWrapper');
+import { Logger } from 'f5-conx-core';
+
+import { acc } from './accWrapper'
+import { EventEmitter } from 'events';
+
+const logger = Logger.getLogger();
+logger.console = false;
+// delete process.env.F5_CONX_CORE_LOG_LEVEL;
+
+if (!process.env.F5_CONX_CORE_LOG_LEVEL) {
+    // if this isn't set by something else, set it to debug for dev
+    process.env.F5_CONX_CORE_LOG_LEVEL = 'DEBUG';
+}
+
+// create OUTPUT channel
+const f5OutputChannel = window.createOutputChannel('f5');
+
+// make visible
+f5OutputChannel.show();
+
+// inject vscode output into logger
+logger.output = function (log: string) {
+    f5OutputChannel.appendLine(log);
+};
+
+const eventer = new EventEmitter()
+    .on('log-http-request', msg => logger.httpRequest(msg))
+    .on('log-http-response', msg => logger.httpResponse(msg))
+    .on('log-debug', msg => logger.debug(msg))
+    .on('log-info', msg => logger.info(msg))
+    .on('log-warn', msg => logger.warning(msg))
+    .on('log-error', msg => logger.error(msg));
 
 const accPackageJson = require('../acc/package.json');
 
-
 export function activate(context: ExtensionContext) {
 
-    logger.debug(`ACC Details: `, {
-        [accPackageJson.name]: accPackageJson.version
+    logger.info(`ACC Details: `, {
+        name: accPackageJson.name,
+        author: accPackageJson.author,
+        description: accPackageJson.description,
+        version: accPackageJson.version,
+        license: accPackageJson.license,
+        repository: accPackageJson.repository.url
     });
 
 
     context.subscriptions.push(commands.registerCommand('f5.chariot.convert', async () => {
 
-        logger.debug(`f5.chariot.convert called`);
+        logger.info(`f5.chariot.convert called`);
 
         await window.withProgress({
             location: ProgressLocation.Notification,
@@ -48,8 +90,10 @@ export function activate(context: ExtensionContext) {
                     logger.debug(`f5.chariot.convert text found`);
 
                     const { declaration, metaData } = await acc(text);
+
                     // display as3 output in editor
                     displayJsonInEditor(declaration);
+
                     // log all the metadata
                     logger.debug('ACC METADATA', metaData);
                 })
@@ -59,9 +103,6 @@ export function activate(context: ExtensionContext) {
         });
 
     }));
-
-
-
 
 }
 
